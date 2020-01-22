5d109d34e0ef3dc40963e744433906b590e895ce
git11-get <353626343@qq.com>
2020-01-22 11:11:57 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,13 @@
+package com.atguigu.bean;
+
+
+public class Customer {
+
+
+    public static void main(String[] args) {
+        System.out.println("hwoniyou");
+        System.out.println("sdsd");
+    }
+    
+
+}
